6347b62ab3a606c174931ca10eee7f3d6b7171e1
yann <yannlefranc330@gmail.com>
2023-05-02 15:51:34 +0200

    Variable Pi

---
+++ src/Demo.java
@@ -0,0 +1,18 @@
+public class Demo {
+    public static void main( String [] args ) {
+
+        int counter = 10;
+        int witch = 25;
+        double pi;
+
+        String name = "yann";
+
+        pi = 3.1415;
+
+        System.out.println(name);
+        System.out.println(counter);
+        System.out.println(witch);
+        System.out.println(pi);
+
+    }
+}
